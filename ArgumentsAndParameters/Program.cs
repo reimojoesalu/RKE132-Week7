3e51593d578ad7f782431e83139b53e24f10d778
reimo.joesalu <reimo.joesalu@tktk.ee>
2024-04-09 07:15:30 +0300

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Enter a world");
+string userInput = Console.ReadLine();
+
+PrintAnyWorld(userInput); //userInput - argument
+
+
+
+
+static void PrintAnyWorld(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ FunctionsWeek7/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+PrintHello();
+PrintGoodBye();
+
+static void PrintHello() //functions / method
+{
+    Console.WriteLine("Hello, world");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator");
+}
